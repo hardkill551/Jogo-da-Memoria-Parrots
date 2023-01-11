b9f91054d989861dd08f64f1232cad0c822e279c
HardKILL <hardkill194@gmail.com>
2023-01-11 19:58:34 -0300

    Layout feito

---
+++ script.js
@@ -0,0 +1,6 @@
+function virarCarta(clique){
+    const x = clique.querySelector(".front-face")
+    x.classList.add("rota2")
+    const y = clique.querySelector(".back-face")
+    y.classList.add("rota")
+}
