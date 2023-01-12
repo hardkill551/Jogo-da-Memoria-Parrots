fe1c8a6a07ee6abadbece664b0ca0b3d69c2ea24
HardKILL <hardkill194@gmail.com>
2023-01-11 22:58:30 -0300

    Distribuição de cartas e Imagens dos parrots feitos

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function virarCarta(clique){
-    const x = clique.querySelector(".front-face")
-    x.classList.add("rota2")
-    const y = clique.querySelector(".back-face")
-    y.classList.add("rota")
+let quantiaDeCartas = prompt("Com quantas cartas você quer jogar?");
+const cartasDoJogo = document.querySelectorAll(".card");
+while(true){
+    if (quantiaDeCartas%2 == 0 && quantiaDeCartas>3 && quantiaDeCartas<15){
+        break
+    }
+    alert("Insira um valor entre 4 e 14 e par!");
+    quantiaDeCartas = prompt("Com quantas cartas você quer jogar?");
 }
+for (let i = 0; i<quantiaDeCartas;i++){
+    cartasDoJogo[i].classList.remove("esconder");
+}
+cartasDoJogo.forEach(card => {
+    let ramdomPos = Math.floor(Math.random() * quantiaDeCartas);
+    card.style.order = ramdomPos;
+});
+
+
+
+
+
+function virarCarta(clique){
+    const x = clique.querySelector(".front-face");
+    x.classList.add("rotacaoFrente");
+    const y = clique.querySelector(".back-face");
+    y.classList.add("rotacaoTras");
+}
+
+
+
