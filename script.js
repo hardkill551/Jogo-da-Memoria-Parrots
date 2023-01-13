11eaa9011701a77abc5d9e46b35eafec7ecb031b
HardKILL <hardkill194@gmail.com>
2023-01-13 19:41:51 -0300

    Ajustes no código

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let quantiaDeCartas = prompt("Com quantas cartas você quer jogar?");
 let quantiaDeCartas2 = [];
-const cartasDoJogo = document.querySelectorAll(".card");
 let cartasViradas = [];
 let quantiaDeRodadas = 0;
+let cartas = document.querySelector(".cartas");
+let listaComAsCartas = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot",];
+let cliques = 0;
 
 function conferirQuantia(){
     while(true){
@@ -15,8 +17,16 @@ function conferirQuantia(){
 }
 
 function aparecerQuantia(){
-    for (let i = 0; i<quantiaDeCartas;i++){
-        cartasDoJogo[i].classList.remove("esconder");
+    for (let i = 0; i<quantiaDeCartas/2;i++){
+        for(let j =0;j<2;j++){
+        cartas.innerHTML += `        <div class="card" onclick="virarCarta(this)" data-test="card">
+        <div class="front-face face" >
+          <img src="imgs/Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png"  data-test="face-down-image" alt="">
+        </div>
+        <div class="back-face face">
+          <img src="imgs/Arquivos Úteis - Projeto 04 - Parrot Card Game/${listaComAsCartas[i]}.gif" data-test="face-up-image" alt="">
+        </div>`
+        }
     }
 }
 
@@ -28,12 +38,15 @@ function embaralharCartas(){
 }
 
 function virarCarta(clique){
-    const x = clique.querySelector(".front-face");
-    x.classList.add("rotacaoFrente");
-    const y = clique.querySelector(".back-face");
-    y.classList.add("rotacaoTras");
-    quantiaDeRodadas++
-    compararCartas(clique);
+    if (cliques !== clique){
+        const x = clique.querySelector(".front-face");
+        x.classList.add("rotacaoFrente");
+        const y = clique.querySelector(".back-face");
+        y.classList.add("rotacaoTras");
+        quantiaDeRodadas++
+        compararCartas(clique);
+        cliques = clique
+    }
 }
 
 function desvirarCarta(clique){
@@ -45,21 +58,32 @@ function desvirarCarta(clique){
     cartasViradas.pop(clique.querySelector(".rotacaoTras"));
     cartasViradas[cartasViradas.length-1].classList.remove("rotacaoFrente");
     cartasViradas.pop(clique.querySelector(".rotacaoFrente"));
-
 }
 
 function compararCartas(clique){
     cartasViradas.push(clique.querySelector(".rotacaoFrente"));
     cartasViradas.push(clique.querySelector(".rotacaoTras"));
     if (cartasViradas.length==4){
+        desabilitarCartas()
         if(cartasViradas[1].innerHTML!==cartasViradas[3].innerHTML){
             setTimeout(desvirarCarta, 1000, clique);
         }
         if(cartasViradas[1].innerHTML==cartasViradas[3].innerHTML){
             cartasViradas=[];
-            verVitoria()
+            verVitoria();
         }
+        setTimeout(habilitarCartas, 1001);
     }
+}
+
+function desabilitarCartas(){
+    for (let i = 0;i<quantiaDeCartas;i++)
+        cartasDoJogo[i].removeAttribute("onclick");
+}
+
+function habilitarCartas(){
+    for (let i = 0;i<quantiaDeCartas;i++)
+        cartasDoJogo[i].setAttribute("onclick", "virarCarta(this)");
 }
 
 function verVitoria(){
@@ -71,9 +95,10 @@ function verVitoria(){
 }
 
 function alerta(){
-    alert(`Você ganhou em ${quantiaDeRodadas} jogadas!`)
+    alert(`Você ganhou em ${quantiaDeRodadas} jogadas!`);
 }
 
 conferirQuantia();
 aparecerQuantia();
+const cartasDoJogo = document.querySelectorAll(".card");
 embaralharCartas();
